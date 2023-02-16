4a16bbbd8f2e3fbaf9e7d6e5d2cab9b7ce60db20
mugeofrey24 <mugeofrey24@gmail.com>
2023-02-16 15:59:24 -0800

    second changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-include<stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
@@ -6,6 +6,6 @@ include<stdio.h>
  */
 int main(void)
 {
-	printf("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
