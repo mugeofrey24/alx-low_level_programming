02c38c4f2f0f4029d937dc111cddc3433caee9fc
mugeofrey24 <mugeofrey24@gmail.com>
2023-02-17 13:45:07 -0800

    first update

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - my main
+ * Description: hello
+ * Return: yesss
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	for (n = 'A'; n <= 'Z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
