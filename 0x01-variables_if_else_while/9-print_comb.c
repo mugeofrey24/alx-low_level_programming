3662baf2dbd44f1c7b039122d4beb0e5bd8b3123
mugeofrey24 <mugeofrey24@gmail.com>
2023-02-17 14:17:15 -0800

    second update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,13 +3,14 @@
 /**
  * main - my main
  * Description: hello
- * Returns: yesss
+ * Return: yesss
  */
+
 int main(void)
 {
 	int num1;
 
-	for (num1 = 48; num1 <= 57; num1++1)
+	for (num1 = 48; num1 <= 57; num1++)
 	{
 		putchar(num1);
 		if (num1 != 57)
