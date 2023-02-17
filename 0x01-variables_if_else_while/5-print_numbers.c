9410c340c5ea3e7b8b3725991d36661a694c8ef1
mugeofrey24 <mugeofrey24@gmail.com>
2023-02-17 13:52:25 -0800

    first update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - my main
+ * Description: hello
+ * Return: yesss
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}
