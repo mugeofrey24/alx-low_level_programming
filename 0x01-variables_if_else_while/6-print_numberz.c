98b96273f700328da758a23cead86b9326d65119
mugeofrey24 <mugeofrey24@gmail.com>
2023-02-17 13:53:48 -0800

    first update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - my main
+ * Description: hello
+ * Return: yesss
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
