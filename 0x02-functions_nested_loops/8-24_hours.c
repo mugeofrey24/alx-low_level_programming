b4e5a10ad28ccffc41e79d80e7388cbed4601e6f
mugeofrey24 <mugeofrey24@gmail.com>
2023-02-21 13:55:19 -0800

    first update

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,46 @@
+include "main.h"
+
+/**
+ * jack_bauer - Hello
+ * Description: world
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d, e;
+
+	e = 58;
+	a = '0';
+	while (a < '3')
+	{
+		if (a == '2')
+		{
+			e = '4';
+		}
+		b = '0';
+		while (b < e)
+		{
+			c = '0';
+			while (c < '6')
+			{
+				d = '0';
+				while (d < 58)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+					d++;
+				}
+				d = '0';
+				c++;
+			}
+			c = '0';
+			b++;
+		}
+		b = '0';
+		a++;
+	}
+}
