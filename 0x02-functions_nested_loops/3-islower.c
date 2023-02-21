d4478b7885750b5621d02b199e5a419f9a687297
mugeofrey24 <mugeofrey24@gmail.com>
2023-02-21 13:26:53 -0800

    first update

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+include "main.h"
+
+/**
+ * _islower - main fuction
+ * Description: my description
+ * @c: my argument
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		if (c == a)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
